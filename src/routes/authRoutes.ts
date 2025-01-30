2a2adabdcdf9fc023946d0b5a370ecdd1b75e556
BrayanBJ27 <xavi_braian@hotmail.com>
2025-01-30 14:38:06 -0500

    feat: :sparkles: Wrap login routes in async functions for improved error handling and consistency

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -3,7 +3,12 @@ import { loginWithEmail, loginWithFirebase } from '../controllers/authController
 
 const router = Router();
 
-router.post('/login', loginWithEmail);
-router.post('/login/firebase', loginWithFirebase);
+router.post('/login', async (req, res) => {
+    await loginWithEmail(req, res);
+});
+
+router.post('/login/firebase', async (req, res) => {
+    await loginWithFirebase(req, res);
+});
 
 export default router;
