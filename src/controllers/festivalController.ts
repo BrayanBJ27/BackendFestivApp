a7666092a0fa7abc1303ffd9ecf24d68f7223544
BrayanBJ27 <xavi_braian@hotmail.com>
2025-01-30 01:08:34 -0500

    feat: :sparkles: Remove unused function import from festival controller

--- src/controllers/festivalController.ts
+++ src/controllers/festivalController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { getFestivals, getFestivalById, createFestival } from '../models/festivalModel';
+import { getFestivals, createFestival } from '../models/festivalModel';
 
 export const getAllFestivals = async (req: Request, res: Response): Promise<void> => {
     try {
