d2eb5566eb3f518d5d2ce7b6e54c5f4f6fa9584b
BrayanBJ27 <xavi_braian@hotmail.com>
2025-02-16 16:36:45 -0500

    feat: :sparkles: Add location endpoint to retrieve location ID based on city and province

--- src/routes/cityRoutes.ts
+++ src/routes/cityRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { getCities } from '../controllers/cityController';
+import { getCities, getLocationId } from '../controllers/cityController';
 
 const router = Router();
 
 // Endpoint que recibe el parámetro "province" en la query string
 router.get('/list', getCities);
+router.get('/location', getLocationId);
 
 export default router;
