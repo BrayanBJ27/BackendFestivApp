f74387c0e83b51b7543b42be83dfad9220b5aadc
BrayanBJ27 <xavi_braian@hotmail.com>
2025-02-21 12:17:58 -0500

    feat: :sparkles: Add routes to get, update, and delete festivals by ID in festivityRoutes

--- src/routes/festivityRoutes.ts
+++ src/routes/festivityRoutes.ts
@@ -1,9 +1,18 @@
 import { Router } from 'express';
-import { getAllFestivals, createNewFestival } from '../controllers/festivityController';
+import { 
+  getAllFestivals, 
+  getFestivalById, 
+  createNewFestival, 
+  updateFestival, 
+  deleteFestival 
+} from '../controllers/festivityController';
 
 const router = Router();
 
 router.get('/list', getAllFestivals);
+router.get('/:id', getFestivalById);      // Nueva ruta para obtener un festival por id
 router.post('/register', createNewFestival);
+router.put('/:id', updateFestival);         // Nueva ruta para actualizar un festival
+router.delete('/:id', deleteFestival);      // Nueva ruta para eliminar un festival
 
 export default router;
