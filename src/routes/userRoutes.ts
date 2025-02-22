179f15bc4b5c2be9a483a88d453e1161f4ed9db0
BrayanBJ27 <xavi_braian@hotmail.com>
2025-02-22 03:13:43 -0500

    feat: :sparkles: Add user profile retrieval and update routes in userRoutes

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,8 +1,10 @@
 import express from 'express';
-import { registerUser } from '../controllers/userController';
+import { getUserProfile, registerUser, updateUserProfile } from '../controllers/userController';
 
 const router = express.Router();
 
+router.get('/:id', getUserProfile);
 router.post('/register', registerUser);
+router.put('/update/:id', updateUserProfile);
 
 export default router;
